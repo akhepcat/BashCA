2f5fd79813fa5636e62bcba5451b57a098416460
Leif Sawyer <leif@denali.net>
2015-07-22 12:45:09 -0800

    trying to make the intermediate-CA work better...

--- bin/CA-gen.sh
+++ bin/CA-gen.sh
@@ -6,7 +6,12 @@ DO_RETURN=${SHLVL}
 #DBG="echo"
 ##########################
 
-trap do_exit SIGINT SIGTERM SIGKILL SIGQUIT SIGABRT SIGSTOP SIGSEGV
+PASSIN=${CAPASS:+-passin pass:"$CAPASS"}
+PASSOUT=${CAPASS:+-passout pass:"$CAPASS"}
+
+echo "using PASS=$PASS"
+
+trap do_exit SIGTERM SIGKILL SIGQUIT SIGABRT SIGSTOP SIGSEGV 2
 
 do_exit()
 {
@@ -36,7 +41,6 @@ then
 fi
 
 . ${MYDIR}/../conf.d/ca.conf
-
 
 if [ -n "$*" ]
 then
@@ -74,16 +78,16 @@ echo "Creating root certificate"
         echo "<--- Certificate for ${SERVER} --->"
         echo "<--- Certificate for ${DOMAIN}-root --->"
         echo ""
-        echo "ORG='${ORG}', OU='Root Certificate Authority', CN=${DOMAIN}, email=${CONTACT}"
+        echo "ORG='${ORG}', OU='${SHORTORG} Root Certificate Authority', CN=${DOMAIN}, email=${CONTACT}"
         echo ""
         # Check for the private key
-        test -s ${PRIV}/${DOMAIN}-CAcert-key.pem || openssl genrsa -aes256 -out ${PRIV}/${DOMAIN}-CAcert-key.pem 4096
+        test -s ${PRIV}/${DOMAIN}-CAcert-key.pem || openssl genrsa ${PASSOUT} -aes256 -out ${PRIV}/${DOMAIN}-CAcert-key.pem 4096
         test $? -eq 0 || do_error "Couldn't generate the ${DOMAIN}-CAcert-key.pem private certificate"
         # Check for the certificate
-        test -s ${CERTS}/${DOMAIN}-CAcert.pem || ${DBG} openssl req -config ${CACFG} -new ${DIGEST} -x509 -days 3650 -key ${PRIV}/${DOMAIN}-CAcert-key.pem -extensions v3_ca -out ${CERTS}/${DOMAIN}-CAcert.pem
+        test -s ${CERTS}/${DOMAIN}-CAcert.pem || ${DBG} openssl req ${PASSIN} ${PASSOUT} -config ${CACFG} -new ${DIGEST} -x509 -days 3650 -key ${PRIV}/${DOMAIN}-CAcert-key.pem -extensions v3_ca -out ${CERTS}/${DOMAIN}-CAcert.pem
         test $? -eq 0 || do_error "Couldn't generate the ${DOMAIN}-CAcert.pem public certificate"
 
-${DBG}	openssl x509 -in ${CERTS}/${DOMAIN}-CAcert.pem -text -noout
+${DBG}	openssl x509 ${PASSIN} ${PASSOUT} -in ${CERTS}/${DOMAIN}-CAcert.pem -text -noout
 fi
 
 echo ""
@@ -102,20 +106,20 @@ echo "Creating Intermediate Signing certificate"
         echo "<--- Certificate for ${SERVER} --->"
         echo "<--- Certificate for ${DOMAIN}-root --->"
         echo ""
-        echo "ORG='${ORG}', OU='Intermediate Signing Authority', CN=signing.${DOMAIN}, email=${CONTACT}"
+        echo "ORG='${ORG}', OU='${SHORTORG} Intermediate Signing Authority', CN=signing.${DOMAIN}, email=${CONTACT}"
         echo ""
 
 # Create the intermediate private key 
-        test -s ${PRIV}/${DOMAIN}-SIGNcert-key.pem || ${DBG} openssl genrsa -aes256 -out ${PRIV}/${DOMAIN}-SIGNcert-key.pem 4096
+        test -s ${PRIV}/${DOMAIN}-SIGNcert-key.pem || ${DBG} openssl genrsa ${PASSOUT} -aes256 -out ${PRIV}/${DOMAIN}-SIGNcert-key.pem 4096
         test $? -eq 0 || do_error "Couldn't generate the ${DOMAIN}-SIGNcert-key.pem private certificate"
 # Create the intermediate SIGNING request
-        test -s ${CSRS}/${DOMAIN}-SIGNcert.csr || ${DBG} openssl req -config ${SIGNCFG} -key ${PRIV}/${DOMAIN}-SIGNcert-key.pem -extensions v3_ca_req -new ${DIGEST} -out ${CSRS}/${DOMAIN}-SIGNcert.csr
+        test -s ${CSRS}/${DOMAIN}-SIGNcert.csr || ${DBG} openssl req -config ${SIGNCFG} ${PASSIN} ${PASSOUT} -key ${PRIV}/${DOMAIN}-SIGNcert-key.pem -extensions v3_ca_req -new ${DIGEST} -out ${CSRS}/${DOMAIN}-SIGNcert.csr
         test $? -eq 0 || do_error "Couldn't generate the ${DOMAIN}-SIGNcert.pem certificate request"
 # Create the signed SIGNING certificate
-        test -s ${CERTS}/${DOMAIN}-SIGNcert.pem || ${DBG} openssl ca -config ${SIGNCFG} -extensions v3_ca -notext -in ${CSRS}/${DOMAIN}-SIGNcert.csr -out ${CERTS}/${DOMAIN}-SIGNcert.pem 
+        test -s ${CERTS}/${DOMAIN}-SIGNcert.pem || ${DBG} openssl ca -config ${SIGNCFG} ${PASSIN} -extensions v3_ca -notext -in ${CSRS}/${DOMAIN}-SIGNcert.csr -out ${CERTS}/${DOMAIN}-SIGNcert.pem 
         test $? -eq 0 || do_error "Couldn't generate the ${DOMAIN}-SIGNcert.pem certificate"
 
-${DBG}	openssl x509 -in ${CERTS}/${DOMAIN}-SIGNcert.pem -text -noout
+${DBG}	openssl x509 ${PASSIN} ${PASSOUT} -in ${CERTS}/${DOMAIN}-SIGNcert.pem -text -noout
 fi
 
 
@@ -136,23 +140,25 @@ for SERVER in ${HOSTS} ${CUSTOM_HOSTS}
         if [ -s ${CBASE}/${SERVER}.cnf ]
         then
                 CONF=${CBASE}/${SERVER}.cnf
+                PROMPT="-batch"
         else
                 CONF=${SIGNCFG}
+                PROMPT=""
         fi
 
 # Create the server private key 
-        test -s ${PRIV}/${SERVER}-key.pem || ${DBG} openssl genrsa -aes256 -out ${PRIV}/${SERVER}-key.pem 4096
+        test -s ${PRIV}/${SERVER}-key.pem || ${DBG} openssl genrsa ${PASSOUT} -aes256 -out ${PRIV}/${SERVER}-key.pem 4096
         test $? -eq 0 || do_error "Couldn't generate the ${SERVER}-key.pem private certificate"
 
 # Create the server certificate request
-        test -s ${CSRS}/${SERVER}.csr || ${DBG} openssl req -config ${CONF} -key ${PRIV}/${SERVER}-key.pem -new ${DIGEST} -out ${CSRS}/${SERVER}.csr
+        test -s ${CSRS}/${SERVER}.csr || ${DBG} openssl req -config ${CONF} ${PROMPT} ${PASSIN} ${PASSOUT} -key ${PRIV}/${SERVER}-key.pem -new ${DIGEST} -out ${CSRS}/${SERVER}.csr
         test $? -eq 0 || do_error "Couldn't generate the ${SERVER}-key.pem private certificate"
 
 # Create the signed certificate
-        test -s ${CERTS}/${SERVER}.crt || ${DBG} openssl ca -config ${CONF} -notext -in ${CSRS}/${SERVER}.csr -out ${CERTS}/${SERVER}.crt 
+        test -s ${CERTS}/${SERVER}.crt || ${DBG} openssl ca -config ${CONF} ${PROMPT} ${PASSIN} -notext -in ${CSRS}/${SERVER}.csr -out ${CERTS}/${SERVER}.crt 
         test $? -eq 0 || do_error "Couldn't generate the ${SERVER}.pem public certificate"
 
-${DBG}	openssl x509 -in ${CERTS}/${SERVER}.crt -text -noout
+${DBG}	openssl x509 ${PASSIN} -in ${CERTS}/${SERVER}.crt -text -noout
         test $? -eq 0 || exit
         echo "----"
         echo ""
@@ -178,18 +184,18 @@ for EMAIL in ${EMAIL_CERTS}
 	DAYS="-days ${DAYS}"
 
 # Create the email private key 
-        test -s ${PRIV}/${EMAIL}-key.pem || ${DBG} openssl genrsa -des3 -out ${PRIV}/${EMAIL}-key.pem 4096
+        test -s ${PRIV}/${EMAIL}-key.pem || ${DBG} openssl genrsa ${PASSOUT} -des3 -out ${PRIV}/${EMAIL}-key.pem 4096
         test $? -eq 0 || do_error "Couldn't generate the ${EMAIL}-key.pem private certificate"
 
 # Create the email certificate request
-        test -s ${CSRS}/${EMAIL}.csr || ${DBG} openssl req -config ${CONF} -key ${PRIV}/${EMAIL}-key.pem -new ${DIGEST} -out ${CSRS}/${EMAIL}.csr
+        test -s ${CSRS}/${EMAIL}.csr || ${DBG} openssl req -config ${CONF} ${PASSIN} ${PASSOUT} -key ${PRIV}/${EMAIL}-key.pem -new ${DIGEST} -out ${CSRS}/${EMAIL}.csr
         test $? -eq 0 || do_error "Couldn't generate the ${EMAIL}-key.pem private certificate"
 
 # Create the email certificate
         if [ ! -s ${CERTS}/${EMAIL}.crt ];
         then
         	export OPENSSL_CONF=${CONF}
-        	${DBG} openssl x509 -req -in ${CSRS}/${EMAIL}.csr -out ${CERTS}/${EMAIL}.crt ${DAYS} \
+        	${DBG} openssl x509 -req -in ${CSRS}/${EMAIL}.csr ${PASSIN} -out ${CERTS}/${EMAIL}.crt ${DAYS} \
         	   -CA ${CERTS}/${DOMAIN}-SIGNcert.pem -CAkey ${PRIV}/${DOMAIN}-SIGNcert-key.pem -CAserial ${BASE}/db/serial \
         	   -setalias "${EMAIL} S/MIME Certificate" -addtrust emailProtection -addreject serverAuth -trustout
 	        test $? -eq 0 || do_error "Couldn't generate the ${EMAIL}.pem public certificate"
@@ -200,11 +206,11 @@ for EMAIL in ${EMAIL_CERTS}
         then
         	export OPENSSL_CONF=${CONF}
         	# should we chain the public root+signing CA certs here?
-		${DBG} openssl pkcs12 -export -in ${CERTS}/${EMAIL}.crt -name "${EMAIL} S/MIME Certificate" -inkey ${PRIV}/${EMAIL}-key.pem -out ${CERTS}/${EMAIL}.p12
+		${DBG} openssl pkcs12 ${PASSIN} ${PASSOUT} -export -in ${CERTS}/${EMAIL}.crt -name "${EMAIL} S/MIME Certificate" -inkey ${PRIV}/${EMAIL}-key.pem -out ${CERTS}/${EMAIL}.p12
 		test $? -eq 0 || do_error "Couldn't generate the ${EMAIL}.pem PKCS12 certificate"
 	fi
 
-${DBG}	openssl x509 -in ${CERTS}/${EMAIL}.crt -text -noout
+${DBG}	openssl x509 ${PASSIN} -in ${CERTS}/${EMAIL}.crt -text -noout
         test $? -eq 0 || exit
         echo "----"
         echo ""
